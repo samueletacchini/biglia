e2b0ecb59d042fa2230ed98c188509aeb19fe263
samueletacchini <tacchinisamuele@gmail.com>
2018-11-29 16:59:44 +0100

    Add files via upload

--- addGame4.php
+++ addGame4.php
@@ -1,18 +1,17 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
-
-
-
-
-
-
-
-header("Location: index.php");
+    $date = $_POST['date'];
+    $result1 = $_POST['result1'];
+    $result2 = $_POST['result2'];
+    $defResult1 = $_POST['defResult1'];
+    $defResult2 = $_POST['defResult2'];
+    $idPlayer1 = $_POST['idPlayer1'];
+    $idPlayer2 = $_POST['idPlayer2'];
+    $idPlayer3 = $_POST['idPlayer3'];
+    $idPlayer4 = $_POST['idPlayer4'];
+    
+    $db = new ConnessioneDb();
+    
+    $query = "INSERT INTO `game4`(`id`, `date`, `result1`, `result2`, `defResult1`, `defResult2`, `player1`, `player2`, `player3`, `player4`) VALUES (, '".$date."', ".$result1.", ".$result2.", ".$defResult1.", ".$defResult2.", ".$player1.", ".$player2.", ".$player3.", ".$player4.")";
+    
+    $db->query($query);
+?>
