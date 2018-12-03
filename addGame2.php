34571177eee9fdebc6b17a555135add718881189
jacopozanti <jacopoz99@gmail.com>
2018-12-03 20:59:12 +0100

    Input game2 e game4

--- addGame2.php
+++ addGame2.php
@@ -1,4 +1,7 @@
 <?php
+	require_once("ConnessioneDb.php");
+	$db = new ConnessioneDb();
+	
     $date = $_POST['date'];
     $result1 = $_POST['result1'];
     $result2 = $_POST['result2'];
@@ -7,9 +10,9 @@
     $idPlayer1 = $_POST['idPlayer1'];
     $idPlayer2 = $_POST['idPlayer2'];
     
-    $db = new ConnessioneDb();
+    $query = "INSERT INTO `game2`(`date`, `result1`, `result2`, `defResult1`, `defResult2`, `player1`, `player2`) VALUES ('{$date}', {$result1}, {$result2}, {$defResult1}, {$defResult2}, {$idPlayer1}, {$idPlayer2})";
     
-    $query = "INSERT INTO `game2`(`id`, `date`, `result1`, `result2`, `defResult1`, `defResult2`, `player1`, `player2`) VALUES (, '".$date."', ".$result1.", ".$result2.", ".$defResult1.", ".$defResult2.", ".$player1.", ".$player2.")";
-    
+	echo $query;
+	
     $db->query($query);
 ?>
--- addGame4.php
+++ addGame4.php
@@ -1,4 +1,7 @@
 <?php
+	require_once("ConnessioneDb.php");
+	$db = new ConnessioneDb();
+	
     $date = $_POST['date'];
     $result1 = $_POST['result1'];
     $result2 = $_POST['result2'];
@@ -6,12 +9,12 @@
     $defResult2 = $_POST['defResult2'];
     $idPlayer1 = $_POST['idPlayer1'];
     $idPlayer2 = $_POST['idPlayer2'];
-    $idPlayer3 = $_POST['idPlayer3'];
+	$idPlayer3 = $_POST['idPlayer3'];
     $idPlayer4 = $_POST['idPlayer4'];
     
-    $db = new ConnessioneDb();
+    $query = "INSERT INTO `game4`(`date`, `result1`, `result2`, `defResult1`, `defResult2`, `player1`, `player2`, `player3`, `player4`) VALUES ('{$date}', {$result1}, {$result2}, {$defResult1}, {$defResult2}, {$idPlayer1}, {$idPlayer2}, {$idPlayer3}, {$idPlayer4})";
     
-    $query = "INSERT INTO `game4`(`id`, `date`, `result1`, `result2`, `defResult1`, `defResult2`, `player1`, `player2`, `player3`, `player4`) VALUES (, '".$date."', ".$result1.", ".$result2.", ".$defResult1.", ".$defResult2.", ".$player1.", ".$player2.", ".$player3.", ".$player4.")";
-    
+	echo $query;
+	
     $db->query($query);
 ?>
